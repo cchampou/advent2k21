5a8025d439ba338c3c25a921f9099be5c923925d
cchampou <clement@champouillon.com>
2021-12-03 13:11:56 +0100

    wip: correct for gamma

--- day3/main.go
+++ day3/main.go
@@ -24,37 +24,39 @@ func main() {
 		binaryValues = append(binaryValues, line)
 	}
 
-	gamma := findGamma(binaryValues, 0, "")
-	epsilon := findEpsilon(gamma)
+	gamma := find(binaryValues, 0, '1', '0')
+	epsilon := find(binaryValues, 0, '0', '1')
 	fmt.Println(gamma, epsilon)
 }
 
-func findGamma(table []string, index int, currentValue string) string {
-	if index > len(table[0]) - 1 {
-		return currentValue
+func find(table []string, index int, char uint8, otherChar uint8) string {
+	if len(table) == 1 || index > len(table[0]) - 1 {
+		return table[0]
 	}
-	totalValue := len(table)
-	freq := 0
+	totalValue := float64(len(table))
+	freq := .0
+	var filter uint8
 	for _, currentBinary := range table {
-		if currentBinary[index] == '1' {
+		if currentBinary[index] == char {
 			freq++
 		}
 	}
-	if freq > totalValue / 2 {
-		return findGamma(table, index + 1, currentValue + "1")
+	if freq >= totalValue / 2 {
+		filter = char
 	} else {
-		return findGamma(table, index + 1, currentValue + "0")
+		filter = otherChar
 	}
+	fmt.Println(freq, totalValue / 2, filter)
+	fmt.Println(filterTable(table, filter, index))
+	return find(filterTable(table, filter, index), index + 1, char, otherChar)
 }
 
-func findEpsilon(gamma string) string  {
-	epsilon := ""
-	for _, currentChar := range gamma {
-		if currentChar == '1' {
-			epsilon += "0"
-		} else {
-			epsilon += "1"
+func filterTable(table []string, filter uint8, index int) []string {
+	var res []string
+	for _, currentValue := range table {
+		if currentValue[index] == filter {
+			res = append(res, currentValue)
 		}
 	}
-	return epsilon
+	return res
 }
